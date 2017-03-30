fcd788cca8ca9476a35076b4398479081dfe6752
myloveann <rj14108@163.com>
2017-03-30 21:50:03 +0800

    Initial commit

---
+++ src/js/tangting_js.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            items:1,
+            loop:true,
+            lazyLoad:true,
+            autoplay:true,
+            autoplayHoverPause:true
+        });
+
+
+
+});
